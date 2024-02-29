114bd4db571da551b8a0d671ad933d1df728702d
Dan Liamco <dan.liamco@yahoo.com>
2024-02-29 15:06:43 +0800

    Addition:  - Runtime Sets now behave a bit more like lists

--- Assets/Common/Scripts/RuntimeSets/RuntimeSet.cs
+++ Assets/Common/Scripts/RuntimeSets/RuntimeSet.cs
@@ -7,7 +7,14 @@ namespace YourCompany.RuntimeSets
     {
         [SerializeField] protected List<T> _items = new List<T>();
 
+        public int count { get { return _items.Count; } }
         public List<T> items { get { return _items; } }
+
+        public T this[int index]
+        {
+            get { return _items[index]; }
+            set { _items[index] = value; }
+        }
 
         public virtual void Add(T item)
         {
